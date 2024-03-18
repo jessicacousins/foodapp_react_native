5602a826d3c9c6b8d6bb7dc12987750417ff2624
Jessica Cousins <113946660+jessicacousins@users.noreply.github.com>
2024-03-17 20:25:30 -0400

    gesture added

--- App.js
+++ App.js
@@ -13,6 +13,8 @@ import EmojiPicker from "./components/EmojiPicker";
 import EmojiList from "./components/EmojiList";
 import EmojiSticker from "./components/EmojiSticker";
 
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
@@ -52,7 +54,7 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer
           placeholderImageSource={PlaceholderImage}
@@ -92,7 +94,7 @@ export default function App() {
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # StickerSmash Tutorial
 
-`npx create-expo-app StickerSmash`
-`npx expo install react-dom react-native-web @expo/metro-runtime`
-`npx expo install @expo/vector-icons`
-`npx expo install expo-image-picker` 
-
 start: `npx expo start`
+
+- `npx create-expo-app StickerSmash`
+- `npx expo install react-dom react-native-web @expo/metro-runtime`
+- `npx expo install @expo/vector-icons`
+- `npx expo install expo-image-picker`
+- `npx expo install react-native-gesture-handler react-native-reanimated`
+
+```
+https://docs.expo.dev/tutorial/introduction/
+```
